938bcf5edfe03ae9fefd42896084aeabbc6ad4e0
InsaneSam01 <wissamchakersultani@gmail.com>
2023-04-03 21:52:45 +0300

    added new canvas for import video results
    check changes.

--- GUI2.py
+++ GUI2.py
@@ -131,9 +131,17 @@ class MyGUI:
         self.select_button = tk.Button(self.select_import_video, text="Select File", command=self.select_file)
         self.select_button.pack()
 
+        #create a frame for two canvases to display the video and result side by side
+        self.display_output = tk.Frame(self.select_import_video)
+        self.display_output.pack(expand=True, fill="both")
+
         # Create canvas for video player
-        self.canvas = tk.Canvas(self.select_import_video, bg="black")
-        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.canvas_video = tk.Canvas(self.display_output, bg="black")
+        self.canvas_video.pack(fill=tk.BOTH, expand=True,side=tk.LEFT, padx=(10,0), pady=10)
+
+        #create canvas for video results
+        self.canvas_video_result = tk.Canvas(self.display_output, bg="black")
+        self.canvas_video_result.pack(fill=tk.BOTH, side=tk.LEFT, expand=True, padx=(10,10), pady=10)
 
         # Create frame for play/pause/skipback/skipforward buttons
         self.buttons_frame = tk.Frame(self.select_import_video)
@@ -150,6 +158,9 @@ class MyGUI:
 
         self.restart_button = tk.Button(self.buttons_frame, text="Restart", command=self.restart)
         self.restart_button.pack(side=tk.LEFT)
+
+        self.analyze_button = tk.Button(self.buttons_frame, text="Analyze Video", command=self.analyze_video)
+        self.analyze_button.pack(side=tk.LEFT)
 
         self.skip_forward_button = tk.Button(self.buttons_frame, text=">>", command=self.skip_forward)
         self.skip_forward_button.pack(side=tk.LEFT)
@@ -173,7 +184,7 @@ class MyGUI:
         self.media_player = vlc.MediaPlayer(file_path)
 
         # Set the media player to display in the canvas
-        self.media_player.set_hwnd(self.canvas.winfo_id())
+        self.media_player.set_hwnd(self.canvas_video.winfo_id())
 
         #WIP set the framerate of the video, needed to advance video frame by frame
         #TODO Figure out how to get this data through parsing
@@ -190,14 +201,14 @@ class MyGUI:
         if self.media_player is not None:
             self.media_player.set_time(0)
             time.sleep(0.1)
-            self.media_player.set_hwnd(self.canvas.winfo_id())
+            self.media_player.set_hwnd(self.canvas_video.winfo_id())
             self.media_player.set_pause(True)
 
 
     def pause(self):
         #pause button functionality
         if self.media_player is not None:
-            self.media_player.pause()
+            self.media_player.set_pause(True)
 
     def skip_back(self):
         #Skip back functionality, works in ms
@@ -210,6 +221,10 @@ class MyGUI:
         if self.media_player is not None:
             current_time = self.media_player.get_time()
             self.media_player.set_time(current_time + self.frame_rate) # skip one frame forward in ms
+
+    def analyze_video(self):
+        new_window = tk.Toplevel(self.window)
+        new_window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
         
     def back_to_main_live_feed(self):
         # Stop the video and hide the live feed and subwindows, and show the original screen
